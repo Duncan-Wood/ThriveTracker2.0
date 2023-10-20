43709c542dbf7f4f8ac25881498b7727040cac05
Duncan <duncanwoodpro@gmail.com>
2023-10-20 18:39:57 -0400

    added template and functionality to the weekly-drink-count page. I now see how I could use React JS to make this more efficient. I currently am using a script element and dom manipulation to updated the page without refreshing, but I am not sure how this will affect when I try to connect this to a database.

--- backend/thrivetracker/thrivetracker_app/urls.py
+++ backend/thrivetracker/thrivetracker_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('introduction/', views.introduction, name='introduction'),
     path('lets-start-personalizing/', views.lets_start_personalizing, name='lets-start-personalizing'),
+    path('weekly-drink-count/', views.weekly_drink_count, name='weekly-drink-count'),
+
 ]
--- backend/thrivetracker/thrivetracker_app/views.py
+++ backend/thrivetracker/thrivetracker_app/views.py
@@ -5,3 +5,15 @@ def introduction(request,):
 
 def lets_start_personalizing(request,):
     return render(request, 'introduction/lets-start-personalizing.html', {})
+
+def weekly_drink_count(request):
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    current_day = "Monday"  # Replace with the actual current day
+    count = 0  # Replace with the actual count
+
+    context = {
+        'days': days,
+        'current_day': current_day,
+        'count': count,
+    }
+    return render(request, 'introduction/weekly-drink-count.html', context)
